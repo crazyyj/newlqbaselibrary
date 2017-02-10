0f56f425e0fb71e7373caa86f8a34803cab56efe
crazyyj <937778927@qq.com>
2017-02-10 15:13:29 +0800

    抽象了基本的自定义View的Dialog 基类

--- newlqbaselibrary/src/main/java/com/andlot/newlqlibrary/base/NewLqDialog.java
+++ newlqbaselibrary/src/main/java/com/andlot/newlqlibrary/base/NewLqDialog.java
@@ -6,6 +6,8 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
@@ -15,6 +17,9 @@ import android.view.ViewGroup;
 import android.view.Window;
 
 import com.andlot.newlqlibrary.R;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /**
  * Created by newlq on 2016/10/13.
@@ -27,40 +32,52 @@ public abstract class NewLqDialog extends DialogFragment {
      *
      */
     protected static final int CREATE_TYPE_DIALOG = 0;
-
     protected static final int CREATE_TYPE_VIEW = 1;
-
     protected int CREATE_TYPE_DEFAULT ;
 
-    public NewLqDialog(int type) {
+    @IntDef({CREATE_TYPE_DIALOG, CREATE_TYPE_VIEW})
+    @Retention(RetentionPolicy.CLASS)
+    public @interface CreateDialogType{}
+
+
+
+    public NewLqDialog(@CreateDialogType int type) {
         CREATE_TYPE_DEFAULT = type;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);  //去掉标题
-        View view = inflater.inflate(getDialogLayoutId(), container);
-        initWidgets(view);
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        int layoutId = getDialogLayoutId();
+        if (layoutId > 0) {
+            getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);  //去掉标题
+            view = inflater.inflate(layoutId, container);
+            initWidgets(view);
+        }
         return view;
     }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        NewLqAlterDialog dialog = getNewlqDialog();
+        Dialog dialog = getNewlqDialog(savedInstanceState);
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+        if (dialog == null) {
+            dialog = super.onCreateDialog(savedInstanceState);
+        }
         return dialog;
     }
 
     protected abstract int getDialogLayoutId();
-    protected abstract int initWidgets(View view);
+    protected int initWidgets(View view){return 0;}
 
-    protected abstract NewLqAlterDialog getNewlqDialog();
-
-
+    protected NewLqAlterDialog getNewlqDialog(Bundle savedInstanceState){return null;}
 
 
-    //T 内部专用提示框
+
+
+    //Tip: 内部专用提示框
     public class NewLqAlterDialog extends AlertDialog{
 
         public NewLqAlterDialog(Context context) {
