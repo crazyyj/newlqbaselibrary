d2c430599b3cf43e71a89ddcb7fa841237a00874
crazyyj <937778927@qq.com>
2017-02-20 11:52:12 +0800

    增加网络接口部分

---
+++ newlqbaselibrary/src/main/java/com/andlot/newlqlibrary/http/IResponse.java
@@ -0,0 +1,15 @@
+package com.andlot.newlqlibrary.http;
+
+/**
+ * Created by sks on 2017/2/20.
+ */
+
+public interface IResponse {
+
+    int CODE_HTTP_OK = 200;
+    int CODE_HTTP_OK1 = 301;
+    int CODE_HTTP_OK2 = 302;
+    int CODE_HTTP_OK3 = 400;
+
+
+}
